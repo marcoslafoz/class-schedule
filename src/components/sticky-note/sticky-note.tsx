99a9c8ad6351113d5b34d944a77a5232f6a299fa
marcoslafoz <marcos.lafoz96@gmail.com>
2024-11-06 17:49:18 +0100

    fetch

--- src/components/sticky-note/sticky-note.tsx
+++ src/components/sticky-note/sticky-note.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './sticky-note.css'
 
+interface Note {
+  note: string
+}
+
 export const StickyNote: React.FC = () => {
-  const [visible, setVisible] = useState(true)
-  const handleClick = () => setVisible(false)
+  const [notes, setNotes] = useState<Note[]>([])
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/marcoslafoz/Horario-ASIR/refs/heads/master/src/assets/json/sticky-notes.json')
+      .then(response => response.json())
+      .then(data => setNotes(data))
+      .catch(error => console.error('Error al cargar los datos:', error))
+  }, [])
 
   return (
-    visible ? (
-      <div className='sticky-note' onClick={handleClick}>
-        <ul className='sticky-list'>
-          <li>sw</li>
-          <li>sw</li>
-          <li>sw</li>
-          <li>sw</li>
-        </ul>
-      </div>
-    ) : null
+    <div className='sticky-note' >
+      <ul className='sticky-list'>
+        {notes.map((note, index) => (
+          <li key={index}>{note.note}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
