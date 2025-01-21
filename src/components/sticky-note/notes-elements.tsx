b25b2e81e9328b8d8d955c1788addb86f4d8bcff
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-21 13:32:59 +0100

    .

--- src/components/sticky-note/notes-elements.tsx
+++ src/components/sticky-note/notes-elements.tsx
@@ -27,6 +27,17 @@ export const NotesElement: React.FC = () => {
           <span>07/02 Recuperación LM</span>
         </div>
       </li>
+
+      <li>
+        <div className='flex flex-row items-center gap-2' >
+          <img className='li-icon' src='https://cdn.icon-icons.com/icons2/2699/PNG/512/minecraft_logo_icon_168974.png' alt='BD icon' />
+          <a target='_blank' href="https://marcoslafoz.notion.site/Servidor-de-Minecraft-17c52e7bcd7d802e964ada99cea1b7bf" rel="noreferrer">
+            <span className='hover:underline' >
+              Minecraft Server
+            </span>
+          </a>
+        </div>
+      </li>
     </>
   )
 }
