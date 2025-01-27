8544524a1bc05ffbdf2ef7bc7949ecde7c1e3222
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-27 11:58:41 +0100

    .

--- src/components/sticky-note/notes-elements.tsx
+++ src/components/sticky-note/notes-elements.tsx
@@ -35,16 +35,16 @@ export const NotesElement: React.FC = () => {
         </div>
       </li>
 
-      {/* <li>
+      <li>
         <div className='flex flex-row items-center gap-2' >
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/15459/15459593.png' alt='BD icon' />
           <a target='_blank' href="https://marcoslafoz.notion.site/Servidor-de-Minecraft-17c52e7bcd7d802e964ada99cea1b7bf" rel="noreferrer">
             <span className='hover:underline' >
-              Minecraft Server
+              MC Server
             </span>
           </a>
         </div>
-      </li> */}
+      </li>
     </>
   )
 }
