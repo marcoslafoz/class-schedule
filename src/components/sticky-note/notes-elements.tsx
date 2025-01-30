0ba31e0b94a466c7cdef2e0a8871990fe9c14e85
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-30 23:08:58 +0100

    initial commit

--- src/components/sticky-note/notes-elements.tsx
+++ src/components/sticky-note/notes-elements.tsx
@@ -31,14 +31,25 @@ export const NotesElement: React.FC = () => {
       <li>
         <div className='flex flex-row items-center gap-2' >
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/330/330425.png' alt='BD icon' />
-          <span>25/02 Gram & Voc</span>
+          <span>25/02 Grammar & Vocabulary </span>
         </div>
       </li>
 
       <li>
         <div className='flex flex-row items-center gap-2' >
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/9074/9074738.png' alt='BD icon' />
-          <span>26/02 List & Writ</span>
+          <span>26/02 Listening &<br/> Writting</span>
+        </div>
+      </li>
+
+      <li>
+        <div className='flex flex-row items-center gap-2' >
+          <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/15459/15459593.png' alt='BD icon' />
+          <a target='_blank' href="https://marcoslafoz.notion.site/Servidor-de-Minecraft-17c52e7bcd7d802e964ada99cea1b7bf" rel="noreferrer">
+            <span className='hover:underline' >
+              MC Server
+            </span>
+          </a>
         </div>
       </li>
     </>
--- src/components/sticky-note/sticky-note.tsx
+++ src/components/sticky-note/sticky-note.tsx
@@ -1,44 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react'
-import clsx from 'clsx'
+import React, { } from 'react'
 import './sticky-note.css'
 import { NotesElement } from './notes-elements'
 
 export const StickyNote: React.FC = () => {
 
-  const [falling, setFalling] = useState(false)
-  const stickyNoteRef = useRef<HTMLDivElement | null>(null)
-
-  const handleDivClick = () => {
-    setFalling(true)
-  }
-
-  useEffect(() => {
-    const element = stickyNoteRef.current
-    if (!element) return
-
-    const handleTransitionEnd = (event: TransitionEvent) => {
-      if (event.propertyName === 'opacity' && falling) {
-        element.style.display = 'none'
-      }
-    }
-
-    element.addEventListener('transitionend', handleTransitionEnd)
-    return () => {
-      element.removeEventListener('transitionend', handleTransitionEnd)
-    }
-  }, [falling])
-
   return (
-    <div
-      ref={stickyNoteRef}
-      className={clsx('sticky-note', { falling })}
-      onClick={handleDivClick}
-    >
+    <div className='sticky-note flex flex-col justify-center'    >
       <ul className="sticky-list">
-
         <NotesElement />
-
-
       </ul>
     </div>
   )
