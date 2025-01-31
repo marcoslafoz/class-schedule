1ad36cd88dd83da41f4ec1527e4714a5a141aa8a
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-31 09:51:11 +0100

    feat: vite gh-pages install

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000
-  },
   plugins: [
     react(),
     VitePWA({
@@ -34,5 +30,4 @@ export default defineConfig({
       },
     }),
   ],
-
-})
+});
