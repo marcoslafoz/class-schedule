dcfeab11021ed6b291d81b862ed719ce8eb76c67
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-31 09:54:24 +0100

    style: pretty all

--- src/modules/components/sticky-note/notes-elements.tsx
+++ src/modules/components/sticky-note/notes-elements.tsx
@@ -2,53 +2,56 @@ import React from 'react'
 import './sticky-note.css'
 
 export const NotesElement: React.FC = () => {
-
   // TODO Hacer que se lea desde un json hosteado en lafoz-server
 
   return (
     <>
       <li>
-        <div className='flex flex-row items-center gap-2' >
+        <div className='flex flex-row items-center gap-2'>
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/9850/9850774.png' alt='BD icon' />
           <span>03/02 Test GBD</span>
         </div>
       </li>
 
       <li>
-        <div className='flex flex-row items-center gap-2' >
+        <div className='flex flex-row items-center gap-2'>
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/689/689317.png' alt='BD icon' />
           <span>05/02 Exámen ISO</span>
         </div>
       </li>
 
       <li>
-        <div className='flex flex-row items-center gap-2' >
+        <div className='flex flex-row items-center gap-2'>
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/919/919827.png' alt='BD icon' />
           <span>07/02 Recuperación LM</span>
         </div>
       </li>
 
       <li>
-        <div className='flex flex-row items-center gap-2' >
+        <div className='flex flex-row items-center gap-2'>
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/330/330425.png' alt='BD icon' />
           <span>25/02 Grammar & Vocabulary </span>
         </div>
       </li>
 
       <li>
-        <div className='flex flex-row items-center gap-2' >
+        <div className='flex flex-row items-center gap-2'>
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/9074/9074738.png' alt='BD icon' />
-          <span>26/02 Listening &<br/> Writting</span>
+          <span>
+            26/02 Listening &<br /> Writting
+          </span>
         </div>
       </li>
 
       <li>
-        <div className='flex flex-row items-center gap-2' >
+        <div className='flex flex-row items-center gap-2'>
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/15459/15459593.png' alt='BD icon' />
-          <a target='_blank' href="https://marcoslafoz.notion.site/Servidor-de-Minecraft-17c52e7bcd7d802e964ada99cea1b7bf" rel="noreferrer">
-            <span className='hover:underline' >
-              MC Server
-            </span>
+          <a
+            target='_blank'
+            href='https://marcoslafoz.notion.site/Servidor-de-Minecraft-17c52e7bcd7d802e964ada99cea1b7bf'
+            rel='noreferrer'
+          >
+            <span className='hover:underline'>MC Server</span>
           </a>
         </div>
       </li>
--- src/modules/components/sticky-note/sticky-note.tsx
+++ src/modules/components/sticky-note/sticky-note.tsx
@@ -1,12 +1,11 @@
-import React, { } from 'react'
+import React from 'react'
 import './sticky-note.css'
 import { NotesElement } from './notes-elements'
 
 export const StickyNote: React.FC = () => {
-
   return (
-    <div className='sticky-note flex flex-col justify-center'    >
-      <ul className="sticky-list">
+    <div className='sticky-note flex flex-col justify-center'>
+      <ul className='sticky-list'>
         <NotesElement />
       </ul>
     </div>
