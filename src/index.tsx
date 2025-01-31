ac1ed6c1ebe6a2a0a4888b402db3cf027d79845d
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-31 10:44:46 +0100

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import '../src/assets/scss/main.scss'
 import { App } from './modules/App.tsx'
+import React from 'react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
