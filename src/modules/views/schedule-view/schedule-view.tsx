3ccb2e2da42b01c1d931780b40dc517441a651df
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-02 23:47:49 +0100

    fix: github-button position absolute

--- src/modules/views/schedule-view/schedule-view.tsx
+++ src/modules/views/schedule-view/schedule-view.tsx
@@ -3,19 +3,21 @@ import { StickyNote, Counter, Schedule, GitHubButton } from '../../components'
 
 export const ScheduleView: React.FC = () => {
   return (
-    <div className='schedule-view-container flex flex-col lg:flex-row justify-center min-h-screen'>
-      <GitHubButton />
+    <>
+      <div className='schedule-view-container flex flex-col lg:flex-row justify-center min-h-screen'>
 
-      <div className='order-2 lg:order-1 lg:w-1/3 p-4 flex flex-col lg:mt-0 mt-10 justify-center items-center lg:items-end '>
-        <StickyNote />
+        <div className='order-2 lg:order-1 lg:w-1/3 p-4 flex flex-col lg:mt-0 mt-10 justify-center items-center lg:items-end '>
+          <GitHubButton />
+          <StickyNote />
+        </div>
+
+        <div className='columna order-1 lg:order-2 lg:w-full flex flex-col items-center p-4'>
+          <Counter />
+          <Schedule />
+        </div>
+
+        <div className='hidden lg:block lg:order-3 lg:w-1/3 p-4' />
       </div>
-
-      <div className='columna order-1 lg:order-2 lg:w-full flex flex-col items-center p-4'>
-        <Counter />
-        <Schedule />
-      </div>
-
-      <div className='hidden lg:block lg:order-3 lg:w-1/3 p-4' />
-    </div>
+    </>
   )
 }
