193acef413dcfbf5c1bcdf73c4cb141d8c55a577
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-03 13:19:56 +0100

    notes: update

--- src/modules/components/sticky-note/notes-elements.tsx
+++ src/modules/components/sticky-note/notes-elements.tsx
@@ -8,13 +8,6 @@ export const NotesElement: React.FC = () => {
     <>
       <li>
         <div className='flex flex-row items-center gap-2'>
-          <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/9850/9850774.png' alt='BD icon' />
-          <span>03/02 Test GBD</span>
-        </div>
-      </li>
-
-      <li>
-        <div className='flex flex-row items-center gap-2'>
           <img className='li-icon' src='https://cdn-icons-png.flaticon.com/512/689/689317.png' alt='BD icon' />
           <span>05/02 Exámen ISO</span>
         </div>
