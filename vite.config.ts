a7240f84d752d93ecbbdbc82dc0648d6fbad1747
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-10 18:02:56 +0100

    build: no caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html'
 import { version } from './package.json'
 
 const hash = Date.now();
 
 export default defineConfig({
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          version: version,
-          hash: hash
-        }
-      }
-    })
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
