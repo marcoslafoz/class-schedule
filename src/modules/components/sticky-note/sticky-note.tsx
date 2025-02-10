66e643d73de20a2e1ccb5ab0e941e4967ee3779d
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-10 19:14:44 +0100

    feat: Sync notes button

--- src/modules/components/sticky-note/sticky-note.tsx
+++ src/modules/components/sticky-note/sticky-note.tsx
@@ -2,12 +2,25 @@ import React from 'react'
 import './sticky-note.css'
 import { NotesElement } from './notes-elements'
 import { AddNoteButton } from './add-note'
+import { Tooltip } from '@heroui/react'
+import { NotesContext } from '../../../common/context/notes-provider'
 
 export const StickyNote: React.FC = () => {
+  const { refetch } = React.useContext(NotesContext)
+
+  const handleRefetch = () => {
+    refetch()
+  }
+
   return (
     <div className='sticky-note flex flex-col '>
-      <div className='justify-self-start pt-2.5 pl-3'>
+      <div className='justify-self-start flex flex-row pt-2.5 pl-3 gap-2'>
         <AddNoteButton />
+        <Tooltip content='Actualizar notas' color='foreground' closeDelay={200}>
+          <button onClick={() => handleRefetch()}>
+            <img src='/assets/images/sync.svg' alt='Sync' className='h-4 opacity-20 object-contain hover:opacity-50' />
+          </button>
+        </Tooltip>
       </div>
       <ul className='sticky-list'>
         <NotesElement />
