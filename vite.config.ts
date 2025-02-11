b513486b564e4ce549a764958d80f6dc0db8dd2b
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-11 01:25:49 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { version } from './package.json'
+import react from '@vitejs/plugin-react''
 
 export default defineConfig({
   plugins: [react()]
